08999754b4fa59fd3458d5ae7397ef8eb529dcfd
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-03-03 21:50:59 +0100

    Add test for object and .swh write

---
+++ pkg/swhid/object_test.go
@@ -0,0 +1,36 @@
+package swhid
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestObject(t *testing.T) {
+	object := NewObject("blob", []byte{})
+	assert.Equal(t, []byte("blob 0\000"), object.Bytes())
+	object = NewObject("foo", []byte("bar"))
+	assert.Equal(t, []byte("foo 3\000bar"), object.Bytes())
+}
+
+func TestWriteObjects(t *testing.T) {
+	cwd, err := os.Getwd()
+	assert.Nil(t, err)
+	dir := t.TempDir()
+	err = os.Chdir(dir)
+	assert.Nil(t, err)
+	WriteObjects = true
+	object := NewObject("blob", []byte{})
+	st, err := os.Stat(filepath.Join(dir, ".swh"))
+	assert.Nil(t, err)
+	assert.True(t, st.Mode().IsDir())
+	hash := NewHash(object.Bytes())
+	hex := hash.String()
+	st, err = os.Stat(filepath.Join(dir, ".swh", "objects", hex[0:2], hex[2:]))
+	assert.Nil(t, err)
+	assert.True(t, st.Mode().IsRegular())
+	err = os.Chdir(cwd)
+	assert.Nil(t, err)
+}
