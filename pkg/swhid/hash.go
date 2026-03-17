4699face103d842df4c4d05abc19a0721152d2f8
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-03-17 23:11:09 +0100

    Make it possible to set hash name

--- pkg/swhid/hash.go
+++ pkg/swhid/hash.go
@@ -1,6 +1,8 @@
 package swhid
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"hash"
@@ -11,15 +13,45 @@ import (
 type Hash []byte
 type HashFunc func() hash.Hash
 
+var HashName string = "sha1cd"
 var HashFunction HashFunc = sha1cd.New
 var HashLength = sha1cd.Size * 2
 
+func SetHash(name string) error {
+	switch name {
+	case "sha1":
+		HashName = name
+		HashFunction = sha1.New
+		HashLength = sha1.Size * 2
+		return nil
+	case "sha1cd":
+		HashName = name
+		HashFunction = sha1cd.New
+		HashLength = sha1cd.Size * 2
+		return nil
+	case "sha256":
+		HashName = name
+		HashFunction = sha256.New
+		HashLength = sha256.Size * 2
+		return nil
+	}
+	return fmt.Errorf("Unknown hash: %s", name)
+}
+
 func NewHash(payload []byte) (Hash, error) {
-	hash := HashFunction().(sha1cd.CollisionResistantHash)
+	hash := HashFunction()
+	crhash, ok := hash.(sha1cd.CollisionResistantHash)
 	hash.Write(payload)
-	h, c := hash.CollisionResistantSum([]byte{})
-	if c {
-		return nil, fmt.Errorf("collision detected")
+	var h []byte
+	if ok {
+		var c bool
+		h, c = crhash.CollisionResistantSum([]byte{})
+		if c {
+			return nil, fmt.Errorf("collision detected")
+		}
+	} else {
+		h = hash.Sum([]byte{})
+
 	}
 	return h, nil
 }
--- pkg/swhid/hash_test.go
+++ pkg/swhid/hash_test.go
@@ -17,6 +17,20 @@ func TestHash(t *testing.T) {
 	assert.Equal(t, "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391", hash.String())
 }
 
+func TestHash256(t *testing.T) {
+	old := HashName
+	SetHash("sha256")
+	// SHA-1
+	hash, err := NewHash([]byte{})
+	assert.Nil(t, err)
+	assert.Equal(t, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", hash.String())
+	// gitoid
+	hash, err = NewHash([]byte("blob 0\000"))
+	assert.Nil(t, err)
+	assert.Equal(t, "473a0f4c3be8a93681a267e3b1e9a7dcda1185436fe141f7749120a303721813", hash.String())
+	SetHash(old)
+}
+
 func TestHashDecode(t *testing.T) {
 	hash, err := NewHashFromString("da39a3ee5e6b4b0d3255bfef95601890afd80709")
 	assert.Nil(t, err)
@@ -25,4 +39,17 @@ func TestHashDecode(t *testing.T) {
 	assert.Equal(t, h, hash)
 	_, err = NewHashFromString("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 	assert.Error(t, err)
+}
+
+func TestHashDecode256(t *testing.T) {
+	old := HashName
+	SetHash("sha256")
+	hash, err := NewHashFromString("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+	assert.Nil(t, err)
+	h, err := NewHash([]byte{})
+	assert.Nil(t, err)
+	assert.Equal(t, h, hash)
+	_, err = NewHashFromString("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+	assert.Error(t, err)
+	SetHash(old)
 }
