832bbbc3df32a5df474c5a050022006e58f91344
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-03-18 22:12:15 +0100

    Test path with an empty directory

--- pkg/swhid/path_test.go
+++ pkg/swhid/path_test.go
@@ -21,19 +21,8 @@ func TestContentFromPath(t *testing.T) {
 
 func TestDirectoryFromPath(t *testing.T) {
 	path := t.TempDir()
-	file := filepath.Join(path, "empty")
-	err := os.WriteFile(file, []byte{}, 0o644)
-	assert.Nil(t, err)
 	dir, err := NewDirectoryFromPath(path)
 	assert.Nil(t, err)
 	want := "swh:1:dir:4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 	assert.Equal(t, want, dir.Swhid().String())
-	st, err := os.Stat(path)
-	assert.Nil(t, err)
-	_, err = NewHashFromPath(path, st)
-	assert.Nil(t, err)
-	st, err = os.Stat(file)
-	assert.Nil(t, err)
-	_, err = NewHashFromPath(file, st)
-	assert.Nil(t, err)
 }
