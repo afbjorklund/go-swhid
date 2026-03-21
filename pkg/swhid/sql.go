f6df843ad9aa87c8f901fef38a729f7467c1fd94
Anders F Björklund <anders.f.bjorklund@gmail.com>
2026-03-21 16:33:18 +0100

    Allow compressing in go instead of c

--- pkg/swhid/sql.go
+++ pkg/swhid/sql.go
@@ -3,8 +3,11 @@
 package swhid
 
 import (
+	"bytes"
+	"compress/zlib"
 	"context"
 	"database/sql"
+	"os"
 	"path/filepath"
 
 	"github.com/mattn/go-sqlite3"
@@ -17,20 +20,30 @@ type Database struct {
 }
 
 func NewDatabase(path string) (*Database, error) {
-	abscompress, err := filepath.Abs("compress")
-	if err != nil {
-		return nil, err
-	}
-	// sqlite> .load ./compress
-	sql.Register("sqlite3_with_compress",
-		&sqlite3.SQLiteDriver{
-			Extensions: []string{
-				abscompress,
-			},
-		})
-	db, err := sql.Open("sqlite3_with_compress", path)
-	if err != nil {
-		return nil, err
+	var db *sql.DB
+	_, err := os.Stat("compress.so")
+	if err == nil {
+		abscompress, err := filepath.Abs("compress")
+		if err != nil {
+			return nil, err
+		}
+		// sqlite> .load ./compress
+		sql.Register("sqlite3_with_compress",
+			&sqlite3.SQLiteDriver{
+				Extensions: []string{
+					abscompress,
+				},
+			})
+		db, err = sql.Open("sqlite3_with_compress", path)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// fallback to sqlite3 without compress
+		db, err = sql.Open("sqlite3", path)
+		if err != nil {
+			return nil, err
+		}
 	}
 	ctx := context.TODO()
 	if err := db.PingContext(ctx); err != nil {
@@ -44,12 +57,49 @@ func NewDatabase(path string) (*Database, error) {
 	return &Database{DB: db}, nil
 }
 
+/*
+** from sqlite3:compressFunc (and uncompressFunc)
+**
+** The output is a BLOB that begins with a variable-length integer that
+** is the input size in bytes (the size of X before compression).  The
+** variable-length integer is implemented as 1 to 5 bytes.  There are
+** seven bits per integer stored in the lower seven bits of each byte.
+** More significant bits occur first.  The most significant bit (0x80)
+** is a flag to indicate the end of the integer.
+ */
+
+func varint(n int) []byte {
+	var i, j int
+	x := make([]uint, 8)
+	for i = 4; i >= 0; i-- {
+		x[i] = uint((n >> (7 * (4 - i))) & 0x7f)
+	}
+	for i = 0; i < 4 && x[i] == 0; i++ {
+	}
+	p := make([]byte, 5)
+	for j = 0; i <= 4; i++ {
+		p[j] = byte(x[i])
+		j++
+	}
+	p[j-1] |= 0x80
+	return p[:j]
+}
+
+func compress(data []byte) []byte {
+	var b bytes.Buffer
+	b.Write(varint(len(data)))
+	w, _ := zlib.NewWriterLevel(&b, zlib.BestSpeed)
+	w.Write(data)
+	w.Close()
+	return b.Bytes()
+}
+
 func (db *Database) WriteObject(ctx context.Context, oid []byte, typ string, data []byte) error {
 	_, err := db.DB.ExecContext(ctx,
-		"INSERT OR IGNORE INTO objects (oid, type, length, data) VALUES ($1, $2, $3, compress($4))",
+		"INSERT OR IGNORE INTO objects (oid, type, length, data) VALUES ($1, $2, $3, $4)",
 		oid,
 		typ,
 		len(data),
-		data)
+		compress(data))
 	return err
 }
--- pkg/swhid/sql_test.go
+++ pkg/swhid/sql_test.go
@@ -14,3 +14,10 @@ func TestSQLDatabase(t *testing.T) {
 	_, err := NewDatabase("file:test?mode=memory&cache=shared")
 	assert.Nil(t, err)
 }
+
+func TestVarint(t *testing.T) {
+	assert.Equal(t, []byte{0x80}, varint(0))
+	assert.Equal(t, []byte{0xfb}, varint(123))
+	assert.Equal(t, []byte{0x01, 0x80}, varint(128))
+	assert.Equal(t, []byte{0x01, 0x87}, varint(135))
+}
